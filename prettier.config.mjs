c48f45ffbfd77c1d00551149593050bc12a8e42b
Szymon Kozioł <szymon.koziol@deployed.pl>
2025-03-19 13:00:59 +0100

    Added prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config}
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+};
+
+export default config;
